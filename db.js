51434e4a3317e4f6eab41021a1bbd88c540bf42a
perfectSquare <naeembinadam@yahoo.com>
2022-09-10 08:21:37 +0500

    sold.json

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const accessories = require("./data/accessories");
 const laptops = require("./data/laptops");
-const sold = require("./data/sold");
+const sold = require("./data/sold.json");
 
 module.exports = {
   accessories,
