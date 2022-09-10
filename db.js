07121e8a1879a080e900aca283fd0ee7b6753bf4
perfectSquare <naeembinadam@yahoo.com>
2022-09-10 16:30:53 +0500

    storage monitors

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const accessories = require("./data/accessories");
 const laptops = require("./data/laptops");
+const monitors = require("./data/monitors");
+const storage = require("./data/storage");
 const sold = require("./data/sold");
 const sales = require("./data/sales");
 
 module.exports = {
   accessories,
   laptops,
+  monitors,
+  storage,
   sold,
   sales
 }
