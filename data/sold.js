f980d9b5052f7695b7eae4d2cea957676ed32430
perfectSquare <naeembinadam@yahoo.com>
2022-09-10 08:18:35 +0500

    sold start

---
+++ data/sold.js
@@ -0,0 +1,17 @@
+[
+    {
+      "id":1,
+      "item": "MSI Clutch GM20 Elite Gaming Mouse",
+      "date": "03-05-2022",      
+      "soldId":1,
+      "type":"accessory",      
+    },
+    {
+        "id":1,
+        "item": "MSI GV15 Gaming Laptop",
+        "date": "22-05-2022",      
+        "soldId":3,
+        "type":"laptop",      
+      }
+    ]
+  
--- db.js
+++ db.js
@@ -1,38 +1,9 @@
 const accessories = require("./data/accessories");
 const laptops = require("./data/laptops");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const sold = require("./data/sold");
 
 module.exports = {
   accessories,
   laptops,
-  tags,
-  brands,
-  itemTypes
+  sold
 }
-
-
-// const items = require("./data/items");
-
-// module.exports = () => {
-
-//   var tags = items.map((i) => i.tags);
-//   tags = [].concat.apply([], tags);
-//   tags = [...new Set(tags)];
-
- 
-
-//   var brands = items.map((i) => i.manufacturer);
-//   brands = [...new Set(brands)];
-
-//   var itemTypes = items.map((i) => i.itemType);
-//   itemTypes = [...new Set(itemTypes)];
-
-//   return {
-//     items,
-//     tags,
-//     brands,
-//     itemTypes,
-//   };
-// };
